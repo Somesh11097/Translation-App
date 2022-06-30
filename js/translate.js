8d7fd094b9e9b6a8e6fc74976e9ee68fff2ec69c
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2022-06-30 15:54:52 +0530

    commit
    Added all the javascript

--- js/translate.js
+++ js/translate.js
@@ -1,11 +1,40 @@
 var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input');
+var outputDiv = document.querySelector('#output');
+
+ var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
 
 // console.log(txtInput);
 
-function clickEventhandler(){
-    console.log("clicked");
-    console.log("input", txtInput.value);
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Something wrong with server");
 }
+function clickEventhandler(){
+    // outputDiv.innerText = "translated " + txtInput.value;
+
+    var inputText = txtInput.value //taking input
+
+
+    //calling server
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    } )
+    .catch(errorHandler)
+
+    // console.log("clicked");
+    // console.log("input", txtInput.value);
+};
 // this is used to add eventlistner to the btnTranslate, so bascially whenever we click on the btn a function is passed with it
+
 btnTranslate.addEventListener("click",  clickEventhandler)
